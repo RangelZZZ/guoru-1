fd3d13fff7c87efde6ce38cdfadfabb74662606d
RangelZZZ <18792561236@163.com>
2016-06-27 00:07:59 +0800

    guoru

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
-var products = require('./products.js');
+var products = require('./routes/products.js');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var maxIdUtils = require('./max-id-utils.js');
+var maxIdUtils = require('./routes/max-id-utils.js');
 maxIdUtils.initMaxIdFile();
 
-app.use('/products', require('./get-one-product.js'));
-app.use('/products', require('./get-all-products.js'));
-app.use('/products', require('./add-product.js'));
-app.use('/products', require('./delete-product.js'));
-app.use('/products', require('./updata-products.js'));
+app.use('/products', require('./routes/get-one-product.js'));
+app.use('/products', require('./routes/get-all-products.js'));
+app.use('/products', require('./routes/add-product.js'));
+app.use('/products', require('./routes/delete-product.js'));
+app.use('/products', require('./routes/updata-products.js'));
 
 app.use(function (err, req, res, next) {
     console.dir(err);
